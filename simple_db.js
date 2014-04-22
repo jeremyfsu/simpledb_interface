b3c7de0c4fae301e4614d3dbb6416136b5a779e5
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-04-22 15:53:33 -0400

    work on the main class

--- simple_db.js
+++ simple_db.js
@@ -1,68 +1,63 @@
-var SimpleDB = {
-  $endpoint: 'https://sdb.amazonaws.com',
+function SimpleDB(accessKeyId, secretKey, domain) {
+  this.endpoint = 'https://sdb.amazonaws.com',
+  this.domain = domain;
+  this.accessKeyId = accessKeyId;
+  this.secretKey = secretKey;
+  this.host = this.endpoint.replace(/.*:\/\//, "");
+  this.version = "2009-04-15";
+}
 
-  init: function(accessKeyId, secretKey, domain) {
-    this.domain = domain;
-    this.accessKeyId = accessKeyId;
-    this.secretKey = secretKey;
-    this.host = $endpoint.replace(/.*:\/\//, "");
-    this.version = "2009-04-15";
-  },
+SimpleDB.prototype.sign = function(params) {
+  var payload = null;
+  var displayUri = this.endpoint;
+  var signer = new AWSV2Signer(this.accessKeyId, this.secretKey);
+  params = signer.sign(params, new Date(), {
+    "verb": "GET",
+    "host": this.host,
+    "uriPath": "/"
+  });
 
-  save: function(item, data) {
-    var params = {};
-
-    params["ItemName"] = item;
-    params["Attribute.1.Name"] = "data";
-    params["Attribute.1.Value"] = data;
-    params["Attribute.1.Replace"] = "true";
-    params["DomainName"] = this.domain;
-    params.Action = "PutAttributes";
-    params.Version = this.version;
-
-    return sign(params);
-  },
-
-  get: function(item) {
-    var params = {};
-
-    params["ItemName"] = item;
-    params["AttributeName.1"] = "data";
-    params["DomainName"] = this.domain;
-    params.Action = "GetAttributes";
-    params.Version = this.version;
-    
-    return sign(params);
-  },
-
-  del: function(item) {
-  },
-
-  sign: function(params) {
-    var payload = null;
-    var displayUri = $endpoint;
-    var signer = new AWSV2Signer(this.accessKeyId, this.secretKey);
-    params = signer.sign(params, new Date(), {
-      "verb": "GET",
-      "host": this.host,
-      "uriPath": "/"
-    });
-    
-    var encodedParams = [];
-    for (var key in params) {
-      if (params[key] !== null) {
-        encodedParams.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
-      } 
-      else {
-        encodedParams.push(encodeURIComponent(key));
-      }
+  var encodedParams = [];
+  for (var key in params) {
+    if (params[key] !== null) {
+      encodedParams.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
+    } 
+    else {
+      encodedParams.push(encodeURIComponent(key));
     }
-
-    payload = encodedParams.join("&");
-    displayUri += "?" + payload;
-
-    return displayUri;
-
   }
-};
-SimpleDB.init();
+
+  payload = encodedParams.join("&");
+  displayUri += "?" + payload;
+
+  return displayUri;
+
+}
+
+SimpleDB.prototype.save = function(item, data) {
+  var params = {};
+
+  params["ItemName"] = item;
+  params["Attribute.1.Name"] = "data";
+  params["Attribute.1.Value"] = data;
+  params["Attribute.1.Replace"] = "true";
+  params["DomainName"] = this.domain;
+  params.Action = "PutAttributes";
+  params.Version = this.version;
+
+  return this.sign(params);
+}
+
+SimpleDB.prototype.get = function(item) {
+  var params = {};
+
+  params["ItemName"] = item;
+  params["AttributeName.1"] = "data";
+  params["DomainName"] = this.domain;
+  params.Action = "GetAttributes";
+  params.Version = this.version;
+
+  return this.sign(params);
+}
+
+SimpleDB.prototype.del = function(item) {}
