0207016c90e3173cf85c63935c82f5feff164345
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-04-22 13:00:45 -0400

    work on get function

--- simple_db.js
+++ simple_db.js
@@ -11,14 +11,36 @@ var SimpleDB = {
 
   save: function(item, data) {
     var params = {};
-    var payload = null;
-    var displayUri = $endpoint;
 
-    params["Attribute.1.Name"] = item;
+    params["ItemName"] = item;
+    params["Attribute.1.Name"] = "data";
     params["Attribute.1.Value"] = data;
     params["Attribute.1.Replace"] = "true";
+    params["DomainName"] = this.domain;
     params.Action = "PutAttributes";
     params.Version = this.version;
+
+    return sign(params);
+  },
+
+  get: function(item) {
+    var params = {};
+
+    params["ItemName"] = item;
+    params["AttributeName.1"] = "data";
+    params["DomainName"] = this.domain;
+    params.Action = "GetAttributes";
+    params.Version = this.version;
+    
+    return sign(params);
+  },
+
+  del: function(item) {
+  },
+
+  sign: function(params) {
+    var payload = null;
+    var displayUri = $endpoint;
     var signer = new AWSV2Signer(this.accessKeyId, this.secretKey);
     params = signer.sign(params, new Date(), {
       "verb": "GET",
@@ -41,12 +63,6 @@ var SimpleDB = {
 
     return displayUri;
 
-  },
-
-  get: function(item) {
-  },
-
-  del: function(item) {
   }
 };
 SimpleDB.init();
